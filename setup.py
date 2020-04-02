b464cb870428c76212c654d3c1a73dbf92d9dfd4
Marc <marchbnr@users.noreply.github.com>
2020-04-02 12:25:04 +0200

    Include microscope static files into the dist package

--- setup.py
+++ setup.py
@@ -42,6 +42,11 @@ setup(
         "gevent",
     ],
     extras_require=extras,
+    package_data={
+        # Include the static files of microscrope into the package.
+        # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
+        "sherlock": ["microscope/static/*"],
+    },
     entry_points={
         "console_scripts": []
     },
