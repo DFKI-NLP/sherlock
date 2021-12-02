e365d8ca0f2f84c95e442669bde0201b3089b080
Leonhard Hennig <leonhard.hennig@dfki.de>
2021-12-02 10:09:03 +0100

    updated to version 0.0.2

--- setup.py
+++ setup.py
@@ -16,15 +16,15 @@ extras["dev"] = extras["testing"] + extras["quality"] + ["torch"]
 
 setup(
     name="sherlock",
-    version="0.0.1a",
-    author="Christoph Alt",
-    author_email="christoph.alt@posteo.de",
+    version="0.0.2",
+    author="Christoph Alt,Leonhard Hennig,Marc Hübner,Gabriel Kressin",
+    author_email="christoph.alt@posteo.de,marc.huebner@dfki.de,leonhard.hennig@dfki.de,gabriel.kressin@dfki.de",
     description="Sherlock - A state-of-the-art information extraction framework",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     keywords="neural information extraction named entity recognition relation extraction entity linking event extraction",
     license="Apache",
-    url="https://github.com/ChristophAlt/sherlock",
+    url="https://github.com/DFKI-NLP/sherlock",
     packages=find_packages(exclude=["*.tests", "*.tests.*",
                                     "tests.*", "tests"]),
     install_requires=[
