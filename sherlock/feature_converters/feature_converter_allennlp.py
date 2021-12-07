f9e0ff483e6cad536b2b0104c58efc9e2cb75a81
Gabriel Kressin <gabriel.kressin@fu-berlin.de>
2021-12-07 15:19:22 +0100

    initial allennlp feature_converter (#8)

---
+++ sherlock/feature_converters/feature_converter_allennlp.py
@@ -0,0 +1,35 @@
+import json
+import os
+from typing import List
+
+from allennlp.data.tokenizers import Tokenizer
+
+from sherlock.feature_converters.feature_converter import FeatureConverter
+
+
+class FeatureConverterAllennlp(FeatureConverter):
+    """
+    Abstract class for FeatureConverters to convert Documents into
+    representation usable for allennlp.
+    """
+
+    def __init__(
+        self,
+        tokenizer: Tokenizer,
+        labels: List[str],
+        max_length: int = 512,
+    ) -> None:
+        super().__init__(labels, max_length)
+        self.tokenizer = tokenizer
+
+    @staticmethod
+    def from_pretrained(path: str, tokenizer: Tokenizer) -> "FeatureConverter":
+        vocab_file = os.path.join(path, "converter_label_vocab.txt")
+        converter_config_file = os.path.join(path, "converter_config.json")
+        with open(converter_config_file, "r", encoding="utf-8") as config_file:
+            config = json.load(config_file)
+        with open(vocab_file, "r", encoding="utf-8") as reader:
+            config["labels"] = [line.strip() for line in reader.readlines()]
+        config["tokenizer"] = tokenizer
+        converter_class = FeatureConverter.by_name(config.pop("name"))
+        return converter_class(**config)
