348404aaac40bed5f962a75d96f2039d330c9baa
Leonhard Hennig <leonhard.hennig@dfki.de>
2021-12-14 13:37:01 +0100

    updated readme and srun params

---
+++ scripts/cluster/binary_relation_clf.sh
@@ -0,0 +1,22 @@
+python ./scripts/run_binary_relation_clf.py \
+  --model_type bert \
+  --model_name_or_path bert-case-uncased \
+  --do_train \
+  --do_eval \
+  --do_predict \
+  --evaluate_during_training \
+  --eval_all_checkpoints \
+  --do_lower_case \
+  --data_dir /ds/text/tacred/data/json/ \
+  --cache_dir ./.cache \
+  --save_steps 8500 \
+  --logging_steps 8500 \
+  --max_seq_length 128 \
+  --per_gpu_eval_batch_size=8 \
+  --per_gpu_train_batch_size=8 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 5.0 \
+  --overwrite_cache \
+  --overwrite_output_dir \
+  --entity_handling mark_entity_append_ner \
+  --output_dir ./experiments
