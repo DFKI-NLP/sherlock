667e71831c9381735f6b51862768ef7789ffdb01
Gabriel Kressin <gabriel.kressin@dfki.de>
2021-12-15 18:39:17 +0100

    allennlp DatasetReader handles generators now

--- sherlock/dataset_readers/dataset_reader_allennlp.py
+++ sherlock/dataset_readers/dataset_reader_allennlp.py
@@ -5,22 +5,15 @@ the Dataset reader.
 Accomplished through the sherlock DatasetReader and
 FeatureConverter
 """
-import os
-import logging
-from typing import Iterable, List, Optional, Dict
+from typing import Iterable, Optional, Dict
 
-import allennlp
 from allennlp.data import Instance
 from allennlp.data.tokenizers import Tokenizer
 from allennlp.data.token_indexers import TokenIndexer
 from allennlp.data import DatasetReader
 
 import sherlock
-from sherlock.document import Document
 from sherlock.tasks import IETask
-
-
-logger = logging.getLogger(__name__)
 
 
 @DatasetReader.register("sherlock_reader")
@@ -44,7 +37,7 @@ class DatasetReaderAllennlp(DatasetReader):
         """
         super().__init__(**kwargs)
 
-        # TODO: make this clean
+        # TODO: make this clean: is it enough to just take the string???
         if task == "binary_rc":
             self.task = IETask.BINARY_RC
         elif task == "ner":
@@ -54,12 +47,11 @@ class DatasetReaderAllennlp(DatasetReader):
 
         DatasetReaderClass = \
             sherlock.dataset_readers.DatasetReader.by_name(dataset_reader_name)
-        # set data_dir later in order to adhere to allennlp DatasetReader api
         self.dataset_reader: sherlock.dataset_readers.DatasetReader = \
-            DatasetReaderClass(data_dir="")
+            DatasetReaderClass()
 
-        # can only initialize FeatureConverter with labels, but labels only
-        # are retrievable with data_dir. Thus, initialize FeatureConverter later
+        # can only initialize FeatureConverter with labels, but labels are only
+        # retrievable given data. Thus, initialize FeatureConverter later
         self.feature_converter_name = feature_converter_name
         self.feature_converter = None
 
@@ -73,47 +65,30 @@ class DatasetReaderAllennlp(DatasetReader):
         self.feature_converter_kwargs["max_length"] = max_tokens
 
 
-    def _read(self, file_path: Optional[str]=None) -> Iterable[Instance]:
-        """Returns iterable of allennlp instances.
+    def _read(
+        self,
+        file_path: Optional[str]=None,
+    ) -> Iterable[Instance]:
+        """Returns iterable of allennlp instances."""
 
-        If sherlock DatasetReader has been initialized with
-        the correct data_dir no file_path is required.
-        TODO: this does not feel clean.
-        """
-        # 1. Set DatasetReader dir
-        if self.dataset_reader.data_dir is "":
-            self.dataset_reader.data_dir = file_path
-            # not clean but can will clean later TODO
-            train_file = "train.json"
-            dev_file = "dev.json"
-            test_file = "test.json"
-            self.dataset_reader.input_files = {
-                split: os.path.join(file_path, filename)
-                for split, filename in zip(["train", "dev", "test"], [train_file, dev_file, test_file])
-            }
-
-        # 2. Init FeatureConverter if that did not happen yet
+        # Initialize FeatureConverter if that did not happen yet
         if self.feature_converter is None:
-
-            labels = self.dataset_reader.get_labels(self.task)
+            # Get class
             FeatureConverterClass = \
                 sherlock.feature_converters.FeatureConverter.by_name(
                     self.feature_converter_name)
+            # Initialize
             self.feature_converter = \
                 FeatureConverterClass(
-                    labels,
+                    labels=self.dataset_reader.get_labels(self.task),
                     framework="allennlp",
                     **self.feature_converter_kwargs
                 )
 
-        # 3. Get Documents
-        logger.info("Reading dataset to documents")
-        # TODO: Maybe make return type of dataset_reader an iterable?
-        documents: List[Document] = self.dataset_reader.get_documents("train")
+        # Get Document generator
+        document_generator = self.dataset_reader.get_documents(file_path)
 
-        # 4. Convert Documents to Instances
-        logger.info("Creating instances")
-        for document in documents:
+        for document in document_generator:
             # TODO: Make return type of document_to_features to iterable
             # (lazy loading -> performance boost)
             input_features = self.feature_converter.document_to_features(document)
