0f7a9ff046ab4d0a6c1bbb75b64cd65533813f09
Leonhard Hennig <leonhardhennig@users.noreply.github.com>
2022-01-03 14:17:12 +0100

    Spacy whitespace handling (#39)
    * removed whitespace replacement
    
    * removed whitespace replacement
    
    * fixed escape character handling
    
    * also remove whitespace tokens
    
    * also remove whitespace tokens
    
    * new version
    
    * new version
    
    * new version
    
    * new version
    
    * new version

--- sherlock/predictors/spacy.py
+++ sherlock/predictors/spacy.py
@@ -15,7 +15,10 @@ logger = logging.getLogger(__name__)
 
 
 LOADED_SPACY_MODELS: Dict[Tuple[str, bool, bool, bool], SpacyModelType] = {}
-
+SPACY_ESCAPE_CHAR_REGEX = re.compile(r"[\t\n\r\f\v]") # we replace these escape chars with "_" before running spacy,
+# so that they get properly tokenized, and discard the corresponding tokens afterwards. Otherwise, we will get
+# harder-to-remove spacy tokens such as '\n \n' or '\t \n \n'. Note: Currently not used.
+WHITESPACE_ONLY_REGEX = re.compile(r"^[ \t\n\r\f\v]+$")
 
 # taken from: https://github.com/allenai/allennlp/blob/master/allennlp/common/util.py
 def get_spacy_model(
@@ -53,11 +56,41 @@ def get_spacy_model(
 
 
 def _remove_spaces(tokens: List[spacy.tokens.Token]) -> List[spacy.tokens.Token]:
+    """
+    Copied from AllenNLP's SpacyTokenizer at
+    https://github.com/allenai/allennlp/blob/main/allennlp/data/tokenizers/spacy_tokenizer.py
+    """
     return [token for token in tokens if not token.is_space]
 
 
+def _remove_escape_char_and_whitespace_tokens(tokens: List[Token]) -> List[Token]:
+    """ Currently not used """
+    def is_whitespace(token: Token) -> bool:
+        return token.lemma == ' ' and token.tag == "_SP"
+    def is_escape_char(token: Token) -> bool:
+        return token.lemma == '_' and SPACY_ESCAPE_CHAR_REGEX.match(token.doc.text[token.start:token.end])
+    return [token for token in tokens if not (is_whitespace(token) or is_escape_char(token))]
+
+
 def _replace_ws(text: str) -> str:
-    return re.sub(r"[\t\n\r\f\v]", "_", text)
+    """
+    Replace escape characters for easier tokenization by Spacy. Currently not used.
+    """
+    return SPACY_ESCAPE_CHAR_REGEX.sub("_", text)
+
+
+def _is_empty_sentence(sent:spacy.tokens.Span) -> bool:
+    """
+
+    Parameters
+    ----------
+    sent  - a sentence
+
+    Returns
+    -------
+    True if the sentence's text consists of whitespace-like characters only.
+    """
+    return WHITESPACE_ONLY_REGEX.match(sent.text) is not None
 
 
 @Predictor.register("spacy")
@@ -94,20 +127,23 @@ class SpacyPredictor(Predictor):
         )
 
     def predict_documents(self, documents: List[Document]) -> List[Document]:
-        spacy_docs = self.spacy.pipe([_replace_ws(doc.text) for doc in documents], n_threads=-1)
+        # following AllenAI's usage of Spacy Tokenizer, i.e. removing spaces after tokenization
+        spacy_docs = self.spacy.pipe([doc.text for doc in documents], n_threads=-1)
         for doc, spacy_doc in zip(documents, spacy_docs):
-            doc.tokens = [Token.from_spacy(doc, token) for token in spacy_doc]
+            doc.tokens = [Token.from_spacy(doc, token) for token in _remove_spaces(spacy_doc)]
             if self.has_sentencizer:
-                doc.sents = [Span(doc, sent.start, sent.end) for sent in spacy_doc.sents]
+                # remove empty sentences
+                doc.sents = [Span(doc, sent.start, sent.end) for sent in spacy_doc.sents if not _is_empty_sentence(sent)]
             for mention in spacy_doc.ents:
                 doc.ments.append(Span.from_spacy(doc, mention))
         return documents
 
     def predict_document(self, document: Document) -> Document:
-        spacy_doc = self.spacy(_replace_ws(document.text))
-        document.tokens = [Token.from_spacy(document, token) for token in spacy_doc]
+        # following AllenAI's usage of Spacy Tokenizer, i.e. removing spaces after tokenization
+        spacy_doc = self.spacy(document.text)
+        document.tokens = [Token.from_spacy(document, token) for token in _remove_spaces(spacy_doc)]
         if self.has_sentencizer:
-            document.sents = [Span(document, sent.start, sent.end) for sent in spacy_doc.sents]
+            document.sents = [Span(document, sent.start, sent.end) for sent in spacy_doc.sents if not _is_empty_sentence(sent)]
         for mention in spacy_doc.ents:
             document.ments.append(Span.from_spacy(document, mention))
         return document
