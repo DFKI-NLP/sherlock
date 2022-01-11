0256c6f7c45fb0243e838085c2d39c8b06449a83
Gabriel Kressin <gabriel.kressin@fu-berlin.de>
2022-01-11 13:39:11 +0100

    ignore_label, documentation, F1 multilabel transformer (#8)

--- sherlock/models/relation_classification/transformer_relation_classifier.py
+++ sherlock/models/relation_classification/transformer_relation_classifier.py
@@ -17,8 +17,7 @@ from allennlp.models import Model
 from allennlp.modules.seq2vec_encoders import BertPooler
 from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
 from allennlp.modules.token_embedders import PretrainedTransformerEmbedder
-from allennlp.modules.transformer import TransformerEmbeddings, TransformerStack, TransformerPooler
-from allennlp.training.metrics import CategoricalAccuracy, FBetaMeasure
+from allennlp.training.metrics import CategoricalAccuracy, FBetaMultiLabelMeasure
 
 
 logger = logging.getLogger(__name__)
@@ -27,14 +26,41 @@ logger = logging.getLogger(__name__)
 @Model.register("transformer_relation_classificatier")
 class TransformerRelationClassifier(Model):
     """
-    This model implements a Relation Classifier based on a Transformer
-    to create embeddings for the inputs.
+    This ``Model`` performs relation classification on a given input.
+
+    It embeds the tokens with a Transformer, takes the embedding of the
+    [CLS] token and classifies based on that onto a relation.
 
     Parameters
     ----------
-    vocab : ``Vocabulary``
-    transformer_model : ``str``, optional (default=``"bert-base-uncased"``)
-        Chooses underlying Transformer based on this parameter.
+    vocab : ``Vocabulary``, required
+        A Vocabulary, required in order to compute sizes for input/output projections.
+        Needs ``"tokens"`` and ``label_namespace`` as namespaces.
+    model_name : ``str``, optional (default=``"bert-base-uncased"``)
+        Chooses underlying Transformer architecture based on this parameter.
+        Has to be an available Huggingface transformer.
+    max_length : ``int``, optional (default=``None``)
+        If set to a number, will limit sequences to maximum length.
+    override_weights_file: `Optional[str]`, optional (default = `None`)
+        If set, this specifies a file from which to load alternate weights
+        that override the weights from huggingface. The file is expected
+        to contain a PyTorch `state_dict`, created with `torch.save()`.
+    num_labels : ``int``, optional (default=``None``)
+        Determines output dimension of Model. If not given, will be determined
+        from `vocab`.
+    label_namespace : ``str``, optional (default=``"labels"``)
+        Gives Namespace for labels within vocabulary.
+    pooler_dropout: ``float``, optional (default=``0.1``)
+        Pooler dropout probability.
+    ignore_label : ``str``, optional (default=``None``)
+        If set, the label is ignored during metrics computation
+        (e.g. for no_relation).
+    f1_average : ``str```, optional (default=``macro``)
+        Averaging method ("micro", "macro", "weighted" or "none") to compute
+        the aggregated F1 score.
+    f1_threshold: `float`, optional (default = `0.5`)
+        Logits over this threshold will be considered predictions for the
+        corresponding class.
     """
 
     def __init__(
@@ -42,10 +68,13 @@ class TransformerRelationClassifier(Model):
         vocab: Vocabulary,
         model_name: str = "bert-base-uncased",
         max_length: Optional[int] = None,
+        override_weights_file: Optional[str] = None,
         num_labels: Optional[int] = None,
         label_namespace: str = "labels",
-        override_weights_file: Optional[str] = None,
         pooler_dropout: float = 0.1,
+        ignore_label : Optional[str] = None,
+        f1_average: str = "macro",
+        f1_threshold: float = 0.5,
         **kwargs,
     ) -> None:
         super().__init__(vocab, **kwargs)
@@ -72,7 +101,15 @@ class TransformerRelationClassifier(Model):
 
         self.loss = torch.nn.CrossEntropyLoss()
         self.acc = CategoricalAccuracy()
-        self.f1 = FBetaMeasure()
+        self.f1_average = f1_average
+        self.f1 = FBetaMultiLabelMeasure(
+            average=f1_average,
+            labels=[
+                k for k, v in self.label_tokens.items() if v != ignore_label],
+            threshold=f1_threshold,
+        )
+        # For FBetaMultiLabelMeasure correct label format computation.
+        self.eye = torch.eye(num_labels)
 
 
     def forward(  # type: ignore
@@ -92,7 +129,8 @@ class TransformerRelationClassifier(Model):
         -------
         An output dictionary consisting of:
         loss : ``torch.FloatTensor``, optional
-            A scalar loss to be optimised. This is only returned when `correct_alternative` is not `None`.
+            A scalar loss to be optimised. This is only returned when `label`
+            is not `None`.
         logits : ``torch.FloatTensor``
             The logits for every possible answer choice
         """
@@ -107,13 +145,17 @@ class TransformerRelationClassifier(Model):
         if label is not None:
             result["loss"] = self.loss(logits, label)
             self.acc(logits, label)
-            self.f1(logits, label)
+            # FBetaMultiLabelMeasure requires boolean labels
+            self.f1(logits, self.eye[label])
 
         return result
 
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
         result = {"acc": self.acc.get_metric(reset)}
-        for metric_name, metrics_per_class in self.f1.get_metric(reset).items():
-            for class_index, value in enumerate(metrics_per_class):
-                result[f"{self.label_tokens[class_index]}-{metric_name}"] = value
+        for metric_name, metric_value in self.f1.get_metric(reset).items():
+            if self.f1_average == "none":
+                for class_index, value in enumerate(metric_value):
+                    result[f"{self.label_tokens[class_index]}-{metric_name}"] = value
+            else:
+                result[metric_name] = metric_value
         return result
