07cac33b3f23b115a1f14599ff08d0839efb36d4
Leonhard Hennig <leonhard.hennig@dfki.de>
2022-05-24 09:36:20 +0200

    added a script to evaluate all languages

---
+++ run_all_langs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for LANG in 'fr' 'ja'
+#for LANG in 'en' 'de' 'es' 'fi' 'fr' 'ru' 'ja' 'zh'
+do
+ for RUN in {1..5}
+ do
+  export SEED=$((1337 * RUN))
+  export RUN_ID=$RUN
+  mkdir -p ./logs/binary_relation_clf/in_language/${LANG}/${RUN}
+  mkdir -p ./logs/binary_relation_clf/cross_language/${LANG}/${RUN}
+  echo 'Running: ' ./scripts/cluster/batch.sh -p RTXA6000 --gpus=1 ./scripts/cluster/wrapper.sh ./scripts/cluster/binary_relation_clf_${LANG}.sh ' run ' ${RUN_ID}
+  ./scripts/cluster/batch.sh -p RTXA6000 --gpus=1 ./scripts/cluster/wrapper.sh ./scripts/cluster/binary_relation_clf_${LANG}.sh 2>&1 > ./logs/binary_relation_clf/in_language/${LANG}/${RUN}/train_evaluate.log
+  echo 'Running: ' ./scripts/cluster/batch.sh -p RTXA6000 --gpus=1 ./scripts/cluster/wrapper.sh ./scripts/cluster/binary_relation_clf_${LANG}_mbert.sh ' run ' ${RUN_ID}
+  ./scripts/cluster/batch.sh -p RTXA6000 --gpus=1 ./scripts/cluster/wrapper.sh ./scripts/cluster/binary_relation_clf_${LANG}_mbert.sh 2>&1 > ./logs/binary_relation_clf/cross_language/${LANG}/${RUN}/train_evaluate.log
+ done
+done
